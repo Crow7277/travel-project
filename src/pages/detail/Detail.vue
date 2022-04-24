c9b5bddef63fb2f51b536d17436bcc0cadb5adfc
Hawke <crow7277@gmail.com>
2022-04-24 19:18:42 +0900

    read list data

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
     <div>
         <DetailBanner></DetailBanner>
         <DetailHeader></DetailHeader>
+        <DetailList :list="list"></DetailList>
         <div class="content"></div>
     </div>
 </template>
@@ -9,9 +10,35 @@
 <script>
 import DetailBanner from './components/Banner.vue';
 import DetailHeader from './components/Header.vue';
+import DetailList from './components/List.vue';
 export default {
     name: 'Detail',
-    components: { DetailBanner, DetailHeader },
+    components: { DetailBanner, DetailHeader, DetailList },
+    data() {
+        return {
+            list: [
+                {
+                    title: '成人票',
+                    children: [
+                        {
+                            title: '成人三馆联票',
+                            children: [
+                                {
+                                    title: '成人三馆联票 - A连锁店销售',
+                                },
+                            ],
+                        },
+                        {
+                            title: '成人五馆联票',
+                        },
+                    ],
+                },
+                { title: '学生票' },
+                { title: '儿童票' },
+                { title: '特惠票' },
+            ],
+        };
+    },
 };
 </script>
 
